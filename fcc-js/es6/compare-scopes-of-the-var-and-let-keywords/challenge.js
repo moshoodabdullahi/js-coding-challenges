36124dfa7cb2ee3b60852e04df20589caf818771
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-07-18 23:31:45 +0100

    feat: add first five fcc es6 coding challenge

--- fcc-js/es6/compare-scopes-of-the-var-and-let-keywords/challenge.js
+++ fcc-js/es6/compare-scopes-of-the-var-and-let-keywords/challenge.js
@@ -1,1 +1,13 @@
-// Solve the challenge in this JavaScript file
+function checkScope() {
+  const i = 'function scope';
+  if (true) {
+    // eslint-disable-next-line no-shadow
+    const i = 'block scope';
+    // eslint-disable-next-line no-console
+    console.log('function scope i is: ', i);
+  }
+  // eslint-disable-next-line no-console
+  console.log('block scope i is: ', i);
+  return i;
+}
+checkScope();
