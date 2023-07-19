59a423c55fd8e755dddfa3214f6724b89b2e79df
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-07-19 10:31:42 +0100

    feat: add prevent object mutation coding challenge

--- fcc-js/es6/prevent-object-mutation/challenge.js
+++ fcc-js/es6/prevent-object-mutation/challenge.js
@@ -1,1 +1,15 @@
-// Solve the challenge in this JavaScript file
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14,
+  };
+  Object.freeze(MATH_CONSTANTS);
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch (ex) {
+    // eslint-disable-next-line no-console
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+// eslint-disable-next-line no-unused-vars
+const PI = freezeObj();
