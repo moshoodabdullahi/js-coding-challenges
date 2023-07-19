ea8ee218c265af242d0cc624a199c1a4280dfd99
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-07-19 15:20:41 +0100

    feat: add write arrow functions with parameters project

--- fcc-js/es6/write-arrow-functions-with-parameters/challenge.js
+++ fcc-js/es6/write-arrow-functions-with-parameters/challenge.js
@@ -1,1 +1,5 @@
-// Solve the challenge in this JavaScript file
+const myConcat = (arr1, arr2) => {
+  return arr1.concat(arr2);
+};
+// eslint-disable-next-line no-console
+console.log(myConcat([1, 2], [3, 4, 5]));
