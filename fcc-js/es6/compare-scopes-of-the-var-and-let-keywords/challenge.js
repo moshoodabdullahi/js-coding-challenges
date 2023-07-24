f4882b3132f40dfad4f973f688b92fe27f960734
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-07-24 10:03:00 +0100

    feat: update compare scopes of the var and let keywords

--- fcc-js/es6/compare-scopes-of-the-var-and-let-keywords/challenge.js
+++ fcc-js/es6/compare-scopes-of-the-var-and-let-keywords/challenge.js
@@ -3,10 +3,8 @@ function checkScope() {
   if (true) {
     // eslint-disable-next-line no-shadow
     const i = 'block scope';
-    // eslint-disable-next-line no-console
     console.log('function scope i is: ', i);
   }
-  // eslint-disable-next-line no-console
   console.log('block scope i is: ', i);
   return i;
 }
