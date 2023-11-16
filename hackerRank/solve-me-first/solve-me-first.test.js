0d68cb5e2bdae140f21dc3015a4ebf9d45e72d61
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-11-16 09:57:57 +0100

    feat: update package.json file, run yarn install and add test.js file

--- hackerRank/solve-me-first/solve-me-first.test.js
+++ hackerRank/solve-me-first/solve-me-first.test.js
@@ -3,3 +3,7 @@ const solveMeFirst = require('./solve-me-first');
 test('adds 1 + 2 to equal 3', () => {
   expect(solveMeFirst(1, 2, '+')).toBe(3);
 });
+
+test('adds 1000 + 100 to equal 1100', () => {
+  expect(solveMeFirst(1000, 100, '+')).toBe(1100);
+});
