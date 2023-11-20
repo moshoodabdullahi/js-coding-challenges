c6de8e76a9685c46eed9db5c602eb52f0c8b6065
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-11-21 00:37:41 +0100

    feat: fix unary operator plusplus error

--- hackerRank/compare-the-triplets/compare-the-triplets.js
+++ hackerRank/compare-the-triplets/compare-the-triplets.js
@@ -2,7 +2,7 @@ const compareTriplets = (a, b) => {
   let aliceScore = 0;
   let bobScore = 0;
 
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 3; i += 1) {
     if (a[i] > b[i]) {
       aliceScore += 1;
     } else if (a[i] < b[i]) {
