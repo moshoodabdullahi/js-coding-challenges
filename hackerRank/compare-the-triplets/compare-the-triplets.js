3a86919bce05f9a945b319782c8b9c722064802e
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-11-22 06:51:16 +0100

    feat: change project to forEach

--- hackerRank/compare-the-triplets/compare-the-triplets.js
+++ hackerRank/compare-the-triplets/compare-the-triplets.js
@@ -2,13 +2,15 @@ const compareTriplets = (a, b) => {
   let aliceScore = 0;
   let bobScore = 0;
 
-  for (let i = 0; i < 3; i += 1) {
-    if (a[i] > b[i]) {
+  a.forEach((alice, i) => {
+    const bob = b[i];
+
+    if (alice > bob) {
       aliceScore += 1;
-    } else if (a[i] < b[i]) {
+    } else if (alice < bob) {
       bobScore += 1;
     }
-  }
+  });
 
   return [aliceScore, bobScore];
 };
